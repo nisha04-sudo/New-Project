831710b7462826d4e68f4188b60e5458f3616d6f
nisha04-sudo <tntnisha24@gmail.com>
2026-06-13 10:02:10 +0530

    added unary.js

---
+++ unary.js
@@ -0,0 +1,9 @@
+var a =30;
+console.log(a++); //post increment
+var b = 20;
+console.log(b--); //post decrement
+var c = 10;
+console.log(++c); //pre increment
+var d = 40;
+console.log(--d);  //pre decrement
+// d value 1st print than add one
