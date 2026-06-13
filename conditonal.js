df92bc5849e547502857222ec4f91eb2fc26a778
nisha04-sudo <tntnisha24@gmail.com>
2026-06-13 09:46:42 +0530

    added conditonal.ja

---
+++ conditonal.js
@@ -0,0 +1,14 @@
+var light= "white";
+if (light=="green"){
+    console.log("go");
+}
+else if (light=="yellow"){
+    console.log("slow down");
+}
+else if (light=="red"){
+    console.log("stop");
+}
+else{
+    console.log("invalid light");
+}   
+console.log("end of program");
